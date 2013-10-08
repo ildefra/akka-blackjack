7722e8abd9d9ebeacab5e322a8ad249b6c105d3d
ildefra <defranceschi.java@gmail.com>
2013-10-08 11:51:24 +0200

    Javadoc, comments, extract method x3, card dealing

--- src/main/java/com/gtech/abj/BoardActor.java
+++ src/main/java/com/gtech/abj/BoardActor.java
@@ -7,6 +7,7 @@ import akka.actor.Props;
 import akka.actor.UntypedActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+
 import static com.gtech.abj.BJProtocol.*;
 
 
@@ -36,9 +37,10 @@ public BoardActor() {
  * Defines actor's reactions to messages.
  * <ol>
  * <li>
- * {@link HandStart} -> sends {@link AskBet} to all {@link PlayerActor}s
+ * {@link RegisterPlayer} -> adds sender to list, starts the game if not already
+ * started
  * </li>
- * <li>{@link Bet} -> registers bet</li>
+ * <li>{@link Bet} -> registers bet, deal cards if last one</li>
  * </ol>
  */
 @Override
@@ -46,11 +48,12 @@ public void onReceive(final Object message) throws Exception {
     log.debug("received message {}", message);
     if (message instanceof RegisterPlayer) {
         waitingPlayers.add(getSender());
-        if (!gameStarted) startGame();
+        if (!gameStarted) startGame();  //TODO: timer to wait for other players
     } if (message instanceof Bet) {
         handleBet((Bet) message);
     } else unhandled(message);
 }
+
 
 private static final int NO_OF_DECKS = 2;
 private void startGame() {
@@ -64,36 +67,49 @@ private void startGame() {
     }
     waitingPlayers.clear();
     
-    //carta al dealer
-    
+    dealCardTo(dealer);
+    letsBet();
+}
+private void letsBet() {
     for (PlayerData player : playingPlayers) {
         player.ref.tell(new AskBet(), self());
     }
 }
 
+
 private void handleBet(final Bet bet) {
     
     //TODO: timer to kick non-betting players out
     
+    registerBet(bet);
+    
+    boolean everyoneHasBet = true;
+    for (PlayerData player : playingPlayers) {
+        if (player.bet == 0) everyoneHasBet = false;
+    }
+    if (everyoneHasBet) {
+        dealCards();
+    }
+}
+
+private void registerBet(final Bet bet) {
     for (PlayerData player : playingPlayers) {
         if (player.ref.equals(getSender())) {
             player.bet = bet.amount;
         }
         break;
     }
-    
-    boolean everyoneHasBet = true;
-    for (PlayerData player : playingPlayers) {
-        if (player.bet == 0) everyoneHasBet = false;
-    }
-    
-    //if tutti bet dealcards
 }
 
-//dealcards 
+private void dealCards() {
+    for (PlayerData player : playingPlayers) dealCardTo(player);
+    dealCardTo(dealer);
+    for (PlayerData player : playingPlayers) dealCardTo(player);
+}
 
-//carta a tutti
-//carta al dealer
-//carta a tutti
-
+private void dealCardTo(final PlayerData player) {
+    FrenchCard drawnCard = deck.draw();
+    player.cards.add(drawnCard);
+    player.ref.tell(new CardDealt(drawnCard), self());    
+}
 }
