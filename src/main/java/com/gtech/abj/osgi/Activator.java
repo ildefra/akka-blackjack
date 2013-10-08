e7e20c64b6a07348b3c6f74ec640bec6497ab253
ildefra <defranceschi.java@gmail.com>
2013-10-08 20:32:23 +0200

    OSGi bundling

---
+++ src/main/java/com/gtech/abj/osgi/Activator.java
@@ -0,0 +1,18 @@
+package com.gtech.abj.osgi;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+
+public class Activator implements BundleActivator {
+
+@Override
+public void start(final BundleContext bc) throws Exception {
+    //do nothing
+}
+
+@Override
+public void stop(final BundleContext bc) throws Exception {
+    //do nothing
+}
+}
